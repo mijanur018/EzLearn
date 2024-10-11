80b2a5aeb144168f6014953abaf59ba4c6688e00
mijanur018 <130287946+mijanur018@users.noreply.github.com>
2024-10-11 22:20:30 +0530

    Update Header.jsx

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./header.css";
+import "./Header.css";
 import { Link } from "react-router-dom";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { SiStudyverse } from "react-icons/si";
